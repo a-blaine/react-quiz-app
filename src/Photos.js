a530692455a7713e81d25ecb90b08f5fd38fa81f
a-blaine <ai.blaine4@protonmail.com>
2023-06-24 22:01:54 +0100

    Removed background functionality, updated CSS

--- src/Photos.js
+++ src/Photos.js
@@ -1,39 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Photos.css";
 
 export default function Photos({ photos }) {
-  const [background, setBackground] = useState(null);
-
-  function handleClick(bg) {
-    let newBackground = bg;
-    setBackground(newBackground);
-  }
-
-  if (background) {
-    return (
-      <div className="bg-photo">
-        <img src={background} alt="Nature background" />
+  return (
+    <section className="Photos">
+      <p>Pick a background for your quote!</p>
+      <br />
+      <div className="grid">
+        {photos.map((photo, index) => {
+          if (index > 4 && index < 11) {
+            return (
+              <div key={index}>
+                <img src={photo.src.landscape} alt="Relaxing nature" />
+              </div>
+            );
+          }
+        })}
       </div>
-    );
-  } else {
-    return (
-      <section className="Photos">
-        <p>Pick a background for your quote!</p>
-        <br />
-        <div className="grid">
-          {photos.map((photo, index) => {
-            if (index > 4 && index < 11) {
-              return (
-                <div key={index}>
-                  <a href="/" onClick={handleClick(photo.src.landscape)}>
-                    <img src={photo.src.landscape} alt="Relaxing nature" />
-                  </a>
-                </div>
-              );
-            }
-          })}
-        </div>
-      </section>
-    );
-  }
+    </section>
+  );
 }
